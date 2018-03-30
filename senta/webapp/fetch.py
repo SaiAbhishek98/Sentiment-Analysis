bf7f1b8248f868cf96274c7f015f27a53f8538ef
Magic <sai.abhishek.2008@gmail.com>
2018-03-30 10:10:21 +0530

    added fetch and view

--- senta/webapp/fetch.py
+++ senta/webapp/fetch.py
@@ -3,19 +3,22 @@ import tweepy
 from tweepy import OAuthHandler
 from textblob import TextBlob
 
-consumer_key = 'mvzp1bHYFrllRPOlztJr5hAf4'
-consumer_secret = '0a2hQ6EblrCWRd7fPHmgxJterHagfko6MWLgJCOm6kMReiVN28'
-access_token = '463677903-4yL7Lj4WvzD9DOw0JXJWydUwmo6bjCO5nSeJd1k8'
-access_token_secret = 'VwT17ydddNmF8s6yfNO2KXFfLTqy32gfnP9SUVZCgSn0C'
+consumer_key = "loPKijQxm9HvnFvW47TYKYMx3"
+consumer_secret = "KMPdYuiZD3qsRg9hI3Zvhk38UIsaQa01fpZp6etLUN1FTEPIaQ"
+access_key = "593403483-XUxnDy6azxMojMuCwowo8gtZQ36u0AF5TNEk5d6j"
+access_secret = "YDvzRQ3jeJinaUYZpPEkoOzOMBclLaBMzEzHGyw2iNcBR"
 
-def fetch_tweets(request):
+def fetch_tweets():
     auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth)
     tweets = api.search(q='Telangana',count = '100')
+    fetched_tweets = []
     for tweet in tweets:
+        fetched_tweets.append(tweet.text + ',' + TextBlob(tweet.text).sentiment.polarity + '<br>') 
         print(tweet.text)
         print(TextBlob(tweet.text).sentiment.polarity)
 
+    return fetched_tweets
 
         
--- senta/webapp/views.py
+++ senta/webapp/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render 
 from . import fetch
 
 
 def index(request):
     return render(request, 'index.html', {})
 
+def print_tweets(request):
+    fetched_tweets = fetch.fetch_tweets()
+    return HttpResponse(fetched_tweets)
